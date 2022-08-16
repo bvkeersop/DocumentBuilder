923104047bce9028bb5c59fce0d99116e153b9d6
Bart van Keersop <bart.van.keersop@gmail.com>
2022-08-16 08:39:19 +0200

    fixed ExcelTable test

--- Domain.Test.Unit/Model/Excel/Table.Excel.Tests.cs
+++ Domain.Test.Unit/Model/Excel/Table.Excel.Tests.cs
@@ -1,5 +1,8 @@
 ﻿using DocumentBuilder.Domain.Extensions;
+using DocumentBuilder.Domain.Model.Excel;
+using DocumentBuilder.Domain.Model.Generic;
 using DocumentBuilder.Domain.Options;
+using DocumentBuilder.Domain.Utilities;
 using FluentAssertions;
 
 namespace DocumentBuilder.Domain.Test.Unit.Model.Excel
@@ -18,9 +21,23 @@ namespace DocumentBuilder.Domain.Test.Unit.Model.Excel
 
             // Assert
             var expectedExcelTableCells = _tableWithoutHeaderAttributes.TableCells.Select(t => t.ToExcelTableCell());
+            var tableCells = _tableWithoutHeaderAttributes.TableCells.OrderBy(e => e.Value);
+            excelTableCells = excelTableCells.OrderBy(e => e.Value);
+            excelTableCells.Count().Should().Be(tableCells.Count());
 
-            //TODO: fix test
-            //excelTableCells.SequenceEqual(expectedExcelTableCells).Should().BeTrue();
+            for (var i = 0; i < excelTableCells.Count(); i++)
+            {
+                var tableCell = tableCells.ElementAt(i);
+                var excelTableCell = excelTableCells.ElementAt(i);
+                AssertAreEqual(excelTableCell, tableCell);
+            }
+        }
+
+        private static void AssertAreEqual(ExcelTableCell excelTableCell, TableCell tableCell)
+        {
+            tableCell.Value.Should().Be(excelTableCell.Value);
+            tableCell.RowPosition.Should().Be(excelTableCell.ExcelRowIdentifier - 1);
+            ExcelColumnIdentifierGenerator.GenerateColumnIdentifier(tableCell.ColumnPosition + 1).Should().Be(excelTableCell.ExcelColumnIdentifier);
         }
     }
 }
